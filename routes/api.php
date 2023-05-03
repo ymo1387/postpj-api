bd6dcc408a1f2bf2c2a416c69555ffde01311c4f
YEMINOO <pudding1387@gmail.com>
2023-05-03 13:30:29 +0630

    add sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\UserLoginController;
+use App\Http\Controllers\Api\V1\UserRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=>'v1'], function () {
+    // register and login
+    Route::post('/auth/register', UserRegisterController::class);
+    Route::post('/auth/login', UserLoginController::class);
+
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
